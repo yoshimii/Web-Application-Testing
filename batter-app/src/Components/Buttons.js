585b7ba6cdee68c651757ef4fe25e2a0186662b9
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-05 16:05:33 -0500

    hit logic correct

--- batter-app/src/Components/Buttons.js
+++ batter-app/src/Components/Buttons.js
@@ -2,51 +2,73 @@ import React from 'react';
 
 class Buttons extends React.Component {
 
-    constructor()  {
+    constructor() {
         super();
         this.state = {
 
             strike: null,
             ball: null,
             foul: null,
-            hit: null,
+            hit: null
         };
     }
 
     strike = () => {
-        this.setState({strike: this.state.strike += 1})
+        this.setState({
+            strike: this.state.strike += 1
+        })
         console.log(this.state.strike);
     }
 
     ball = () => {
-        this.setState({ball: this.state.ball += 1})
+        this.setState({
+            ball: this.state.ball += 1
+        })
         console.log(this.state.ball)
     }
 
     foul = () => {
-        if(this.state.strike === 0 || this.state.strike === null || this.state.strike === 1) {
-            this.setState({strike: this.state.strike += 1})
+        if (this.state.strike === 0 || this.state.strike === null || this.state.strike === 1) {
+            this.setState({
+                strike: this.state.strike += 1
+            })
             console.log(this.state.strike)
-        }else if (this.state.strike === 2){
-        this.setState({strike: this.state.strike = 2})
-        console.log(this.state.strike)
+        } else if (this.state.strike === 2) {
+            this.setState({
+                strike: this.state.strike = 2
+            })
+            console.log(this.state.strike)
         }
     }
 
 
     hit = () => {
-        this.setState({hit: this.state.hit += 1})
-        console.log(this.state.hit)
+        this.setState({
+            ball: this.state.ball = 0,
+            strike: this.state.strike = 0
+        })
+        console.log(this.state.ball, this.state.strike)
     }
-
 
 
     render() {
         return([
-        <button onClick={() => this.strike()} className='strike-action'>Strike</button>,
-        <button onClick={() => this.ball()} className='ball-action'>Ball</button>,
-        <button onClick={() =>this.foul()} className='foul-action'>Foul</button>,
-        <button onClick={() =>this.hit()} className='hit-action'>Hit</button>
+            <button onClick={
+                    () => this.strike()
+                }
+                className='strike-action'>Strike</button>,
+            <button onClick={
+                    () => this.ball()
+                }
+                className='ball-action'>Ball</button>,
+            <button onClick={
+                    () => this.foul()
+                }
+                className='foul-action'>Foul</button>,
+            <button onClick={
+                    () => this.hit()
+                }
+                className='hit-action'>Hit</button>
         ])
     }
 }
