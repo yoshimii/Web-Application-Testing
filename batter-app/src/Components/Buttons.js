cb03cccdc32a8486a71c78a80b38c3dc23084bc0
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-05 15:20:07 -0500

    all buttons incrementing on click

--- batter-app/src/Components/Buttons.js
+++ batter-app/src/Components/Buttons.js
@@ -2,12 +2,46 @@ import React from 'react';
 
 class Buttons extends React.Component {
 
+    constructor()  {
+        super();
+        this.state = {
+
+            strike: null,
+            ball: null,
+            foul: null,
+            hit: null,
+        };
+    }
+
+    strike = () => {
+        this.setState({strike: this.state.strike += 1})
+        console.log(this.state.strike);
+    }
+
+    ball = () => {
+        this.setState({ball: this.state.ball += 1})
+        console.log(this.state.ball)
+    }
+
+    foul = () => {
+        this.setState({foul: this.state.foul += 1})
+        console.log(this.state.foul)
+    }
+
+
+    hit = () => {
+        this.setState({hit: this.state.hit += 1})
+        console.log(this.state.hit)
+    }
+
+
+
     render() {
         return([
-        <button onClick={() =>} className=''>Strike</button>,
-        <button onClick={() =>} className=''>Ball</button>,
-        <button onClick={() =>} className=''>Foul</button>,
-        <button onClick={() =>} className=''>Hit</button>
+        <button onClick={() => this.strike()} className='strike-action'>Strike</button>,
+        <button onClick={() => this.ball()} className='ball-action'>Ball</button>,
+        <button onClick={() =>this.foul()} className='foul-action'>Foul</button>,
+        <button onClick={() =>this.hit()} className='hit-action'>Hit</button>
         ])
     }
 }
